78335b0fc53f02b9d95aa6bb2ffc459e6a3babd2
odysian <odyssian7@gmail.com>
2025-11-02 13:03:45 -0500

    add if statement

---
+++ Logs/analyze_logs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOG_DIR="/home/odys/Linux-Learning/Logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+REPORT_FILE="/home/odys/Linux-Learning/Logs/log_analysis_report.txt"
+
+echo "Analyzing log files" > "$REPORT_FILE"
+echo "===================" >> "$REPORT_FILE"
+
+echo -e "\nList of log files updated in last 24 hours" >> "$REPORT_FILE"
+LOG_FILES=$(find . -name "*.log" -mtime -1)
+echo "$LOG_FILES" >> "$REPORT_FILE"
+
+for LOG_FILE in $LOG_FILES; do
+
+    echo -e "\n" >> "$REPORT_FILE"
+    echo "===================================================" >> "$REPORT_FILE"
+    echo "============$LOG_FILE============" >> "$REPORT_FILE"
+    echo "===================================================" >> "$REPORT_FILE"
+
+    for PATTERN in ${ERROR_PATTERNS[@]}; do
+
+        echo -e "\nSearching $PATTERN logs in $LOG_FILE file" >> "$REPORT_FILE"
+        grep "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
+
+        echo -e "\nNumber of $PATTERN logs found in $LOG_FILE" >> "$REPORT_FILE" 
+
+        ERROR_COUNT=$(grep -c "$PATTERN" "$LOG_FILE")
+        echo $ERROR_COUNT >> "$REPORT_FILE"
+
+        if [ "$ERROR_COUNT" -gt 10 ]; then
+            echo -e "\n ⚠️ Action Required: too many $PATTERN errors in log file $LOG_FILE"
+        fi
+    done
+done
+
+echo -e "\nLog analysis completed and report saved in: $REPORT_FILE"
--- README.md
+++ README.md
@@ -7,14 +7,15 @@ Find files in cd with .log that have been modified in past 24hrs:
 - `LOG_FILES=$(find . -name "*.log" -mtime -1)`
 - `echo "$LOG_FILES"`
 
-Created script in vim to [analyze-logs](Logs/analyze-logs.sh)
+Created script in vim to [analyze_logs](Logs/analyze_logs.sh)
 - Add shebang `#!/bin/bash`
 - Add formatting to echo lines with -e and \n
 - Add variables for absolute paths to the logs files
 - Add array variable for "ERROR, FATAL, and CRITICAL"
 - Add multiple for loops to make code more flexible
 - Fix array loop using `${ERROR_PATTERNS[@]}`
-
+- Output results to [log_analysis_report](Logs/log_analysis_report.txt)
+- Add if statement to alert if `$ERROR_COUNT`>10
 
 Give execute perms:  
 `chmod +x analyze.logs.sh`
